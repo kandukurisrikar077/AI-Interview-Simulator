81bd4206add2bddc0f9f3ca0160e8a060d74beee
srikar <kandukurisrikar077@gmail.com>
2026-07-09 14:33:11 +0530

    Temporary React render test

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((reg) => {
-      console.log('IntervueAI PWA ServiceWorker active: ', reg.scope);
-    }).catch((err) => {
-      console.error('ServiceWorker registration error: ', err);
-    });
-  });
+function App() {
+  return (
+    <div
+      style={{
+        color: "white",
+        background: "#111",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+      }}
+    >
+      React is working ✅
+    </div>
+  );
 }
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
