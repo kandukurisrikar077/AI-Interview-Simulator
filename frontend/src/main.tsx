1caf26bcfc9aadff69c8dd076df2b2bf4970575b
srikar <kandukurisrikar077@gmail.com>
2026-07-21 15:51:40 +0530

    Restore original frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-function App() {
-  return (
-    <div
-      style={{
-        color: "white",
-        background: "#111",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "40px",
-      }}
-    >
-      React is working ✅
-    </div>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
